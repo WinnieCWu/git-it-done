39b9c771af7e2aa0080edaadf40c924ee8ad6ce3
WinnieCWu <winniechn7@gmail.com>
2022-02-10 21:24:51 -0800

    converted fetch data to DOM elements

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,3 +1,5 @@
+var issueContainerEl = document.querySelector("#issues-container");
+
 var getRepoIssues = function(repo) {
     console.log(repo);
     //the issues?direction=asc reverses order to return older issues first
@@ -6,7 +8,8 @@ var getRepoIssues = function(repo) {
         // request was successful
         if (response.ok) {
           response.json().then(function(data) {
-            console.log(data);
+              //pass response data to DOM function
+            displayIssues(data);
           });
         }
         else {
@@ -16,3 +19,43 @@ var getRepoIssues = function(repo) {
 };
 
 getRepoIssues("facebook/react");
+
+var displayIssues = function(issues) {
+
+    if (issues.length === 0) {
+        issueContainerEl.textContent = "This repo has no open issues!";
+        return;
+    }
+    
+    issueContainerEl.appendChild(issueEl);
+
+    for (var i = 0; i < issues.length; i++) {
+        //create a link element to take users to the issue on github
+        var issuesEl = document.createElement("a");
+        issuesEl.classList = "list-item flex-row justify-space-between align-center";
+        //the html_url property links to the full issue on GitHub
+        issuesEl.setAttribute("href", issues[i].html_url);
+        //the target="_blank" attribute is added to each <a> element to open issues on new tab
+        issuesEl.setAttribute("target", "_blank");
+
+        // create span to hold issue title
+        var titleEl = document.createElement("span");
+        titleEl.textContent = issues[i].title;
+
+        // append to container
+        issueEl.appendChild(titleEl);
+
+        // create a type element
+        var typeEl = document.createElement("span");
+
+        // check if issue is an actual issue or a pull request
+        if (issues[i].pull_request) {
+        typeEl.textContent = "(Pull request)";
+        } else {
+        typeEl.textContent = "(Issue)";
+        }
+
+        // append to container
+        issueEl.appendChild(typeEl);
+    }
+};
